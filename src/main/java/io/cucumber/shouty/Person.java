e7c32e1fd0610650cebdfd942ee44fa4d8d4621b
Brian Sjoberg <brian_sjoberg@yahoo.com>
2024-06-26 20:43:11 -0400

    Added hamcrest and two more scenarios to handle distance.

--- src/main/java/io/cucumber/shouty/Person.java
+++ src/main/java/io/cucumber/shouty/Person.java
@@ -6,22 +6,27 @@ import java.util.List;
 public class Person {  
     private final List<String> messagesHeard = new ArrayList<String>();
     private final Network network;
+    private final int location;
 
-    public Person(Network network) {
+    public Person(Network network, int location) {
         this.network = network;
+        this.location = location;
         network.subscribe(this);
-    }
-
-    public void shout(String message) {
-        network.broadcast(message);
     }
 
     public List<String> getMessagesHeard() {
         return messagesHeard;
     }
 
+    public void shout(String message) {
+        network.broadcast(message, getLocation());
+    }
+
     public void hear(String message) {
         messagesHeard.add(message);
     }
 
+    public int getLocation() {
+        return location;
+    }
 }
--- src/test/java/io/cucumber/shouty/StepDefs.java
+++ src/test/java/io/cucumber/shouty/StepDefs.java
@@ -5,25 +5,34 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.core.IsIterableContaining.hasItem;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StepDefs {
     private String messageFromSean;
     private Network network;
-    private HashMap<String, Person> people;
+    private Map<String, Person> people;
 
     @Before
     public void createNetwork() {
-        network = new Network();
-        people = new HashMap<>();
+        people = new HashMap<String, Person>();
     }
 
-    @Given("a person named {word}")
-    public void a_person_named(String name) {
-        people.put(name, new Person(network));
+    @Given("the range is {int}")
+    public void the_range_is(int range) throws Throwable {
+        network = new Network(range);
+    }
+
+    @Given("a person named {word} is located at {int}")
+    public void a_person_named_is_located(String name, int location) throws Throwable {
+        people.put(name, new Person(network, location));
     }
 
     @When("Sean shouts {string}")
@@ -34,6 +43,12 @@ public class StepDefs {
 
     @Then("Lucy should hear Sean's message")
     public void lucy_hears_Sean_s_message() throws Throwable {
-        assertEquals(asList(messageFromSean), people.get("Lucy").getMessagesHeard());
+        assertEquals(Collections.singletonList(messageFromSean), people.get("Lucy").getMessagesHeard());
+    }
+
+    @Then("Larry should not hear Sean's message")
+    public void larry_does_not_hear_Sean_s_message() throws Throwable {
+        List<String> heardByLarry = people.get("Larry").getMessagesHeard();
+        assertThat(heardByLarry, not(hasItem(messageFromSean)));
     }
 }
