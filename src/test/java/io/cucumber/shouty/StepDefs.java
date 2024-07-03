586ce3bcc9ce1829eaeccfee3ce7ec0c5e99d1dc
Brian Sjoberg <brian_sjoberg@yahoo.com>
2024-07-03 14:44:17 -0400

    Introduced premium feature.

--- src/test/java/io/cucumber/shouty/StepDefs.java
+++ src/test/java/io/cucumber/shouty/StepDefs.java
@@ -11,6 +11,7 @@ import io.cucumber.java.en.When;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.core.IsIterableContaining.hasItem;
+import static org.hamcrest.core.IsIterableContaining.hasItems;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.ArrayList;
@@ -20,10 +21,11 @@ import java.util.List;
 import java.util.Map;
 
 public class StepDefs {
-    private String messageFromSean;
     private static final int DEFAULT_RANGE = 100;
     private Network network = new Network(DEFAULT_RANGE);
     private Map<String, Person> people;
+    private Map<String, List<String>> messagesShoutedBy;
+
 
     static class Whereabouts {
         public String name;
@@ -43,11 +45,7 @@ public class StepDefs {
     @Before
     public void createNetwork() {
         people = new HashMap<String, Person>();
-    }
-
-    @Given("a person named {word}") 
-    public void a_person_named(String name) throws Throwable {
-        people.put(name, new Person(network, 0));
+        messagesShoutedBy = new HashMap<String, List<String>>();
     }
 
     @Given("the range is {int}")
@@ -55,59 +53,119 @@ public class StepDefs {
         network = new Network(range);
     }
 
-    @Given("people located at")
-    public void people_located_at(@Transpose List<Whereabouts> whereabouts) {
-        for (Whereabouts whereabout : whereabouts) {
+    @Given("a person named {word}")
+    public void a_person_named(String name) throws Throwable {
+        people.put(name, new Person(network, 0));
+    }
+
+    @Given("people are located at")
+    public void people_are_located_at(@Transpose List<Whereabouts> whereabouts) {
+        for (Whereabouts whereabout : whereabouts ) {
             people.put(whereabout.name, new Person(network, whereabout.location));
-        }    
+        }
+    }
+
+    @Given("Sean has bought {int} credits")
+    public void sean_has_bought_credits(int credits) {
+        people.get("Sean").setCredits(credits);
     }
 
     @When("Sean shouts")
     public void sean_shouts() throws Throwable {
-        people.get("Sean").shout("Hello, world");
+        shout("Hello, world");
     }
 
     @When("Sean shouts {string}")
-    public void sean_shouts(String message) throws Throwable {
-        people.get("Sean").shout(message);
-        messageFromSean = message;
+    public void sean_shouts_message(String message) throws Throwable {
+        shout(message);
+    }
+
+    @When("Sean shouts {int} messages containing the word {string}")
+    public void sean_shouts_messages_containing_the_word(int count, String word) throws Throwable {
+        String message = "a message containing the word " + word;
+        for (int i = 0; i < count; i++) {
+            shout(message);
+        }
     }
 
     @When("Sean shouts the following message")
-    public void sean_shouts_the_following_message(String message) {
+    public void sean_shouts_the_following_message(String message) throws Throwable {
+        shout(message);
+    }
+
+    @When("Sean shouts a message")
+    public void sean_shouts_a_message() throws Throwable {
+        shout("here is a message");
+    }
+
+    @When("Sean shouts a long message")
+    public void sean_shouts_a_long_message() throws Throwable {
+        String longMessage = String.join(
+                "\n",
+                "A message from Sean",
+                "that spans multiple lines");
+        shout(longMessage);
+    }
+
+    @When("Sean shouts {int} over-long messages")
+    public void sean_shouts_some_over_long_messages(int count) throws Throwable {
+        String baseMessage = "A message from Sean that is 181 characters long ";
+        String padding = "x";
+        String overlongMessage = baseMessage + padding.repeat(181 - baseMessage.length());
+
+        for (int i = 0; i < count; i++) {
+            shout(overlongMessage);
+        }
+    }
+
+    private void shout(String message) {
         people.get("Sean").shout(message);
-        messageFromSean = message;
+        List<String> messages = messagesShoutedBy.get("Sean");
+        if (messages == null) {
+            messages = new ArrayList<String>();
+            messagesShoutedBy.put("Sean", messages);
+        }
+        messages.add(message);
+    }
+
+    @Then("Lucy should hear Sean's message")
+    public void lucy_hears_Sean_s_message() throws Throwable {
+        List<String> messages = messagesShoutedBy.get("Sean");
+        assertEquals(messages, people.get("Lucy").getMessagesHeard());
     }
 
     @Then("Lucy should hear a shout")
     public void lucy_should_hear_a_shout() throws Throwable {
         assertEquals(1, people.get("Lucy").getMessagesHeard().size());
     }
-   
+
     @Then("{word} should not hear a shout")
     public void person_should_not_hear_a_shout(String name) throws Throwable {
         assertEquals(0, people.get(name).getMessagesHeard().size());
     }
 
-    @Then("Lucy should hear Sean's message")
-    public void lucy_hears_Sean_s_message() throws Throwable {
-        assertEquals(Collections.singletonList(messageFromSean), people.get("Lucy").getMessagesHeard());
-    }
-
-    @Then("Larry should not hear Sean's message")
-    public void larry_does_not_hear_Sean_s_message() throws Throwable {
-        List<String> heardByLarry = people.get("Larry").getMessagesHeard();
-        assertThat(heardByLarry, not(hasItem(messageFromSean)));
-    }
-
     @Then("Lucy hears the following messages:")
-    public void lucy_hears_the_following_messages(io.cucumber.datatable.DataTable expectedMessages) {
+    public void lucy_hears_the_following_messages(DataTable expectedMessages) {
         List<List<String>> actualMessages = new ArrayList<List<String>>();
         List<String> heard = people.get("Lucy").getMessagesHeard();
         for (String message : heard) {
             actualMessages.add(Collections.singletonList(message));
         }
-
         expectedMessages.diff(DataTable.create(actualMessages));
+    }
+
+    @Then("Lucy hears all Sean's messages")
+    public void lucy_hears_all_Sean_s_messages() throws Throwable {
+        List<String> heardByLucy = people.get("Lucy").getMessagesHeard();
+        List<String> messagesFromSean = messagesShoutedBy.get("Sean");
+
+        // Hamcrest's hasItems matcher wants an Array, not a List.
+        String[] messagesFromSeanArray = messagesFromSean.toArray(new String[messagesFromSean.size()]);
+        assertThat(heardByLucy, hasItems(messagesFromSeanArray));
+    }
+
+    @Then("Sean should have {int} credits")
+    public void sean_should_have_credits(int expectedCredits) {
+        assertEquals(expectedCredits, people.get("Sean").getCredits());
     }
 }
